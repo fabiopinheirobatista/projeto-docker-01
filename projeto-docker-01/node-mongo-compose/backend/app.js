90ce81ad69bbd95b0060c15aacfba987ee212bb8
Fabio <42181125+fabiopinheirobatista@users.noreply.github.com>
2022-02-03 20:32:21 -0300

    Update app.js

--- projeto-docker-01/node-mongo-compose/backend/app.js
+++ projeto-docker-01/node-mongo-compose/backend/app.js
@@ -2,13 +2,30 @@ const express = require('express')
 const restful = require('node-restful')
 const server = express()
 const mongoose = restful.mongoose
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
 // database
 mongoose.Promise = global.Promise
 mongoose.connect('mongodb://db/mydb')
 
-// teste
-server.get('/', (req, res, next) => res.send('Back-end'))
+// middlewares
+server.use(bodyParser.urlencoded({extended: true}))
+server.use(bodyParser.json())
+server.use(corsrs())
+
+// ownerDocumentconst
+const Client = restful.model('Client', {
+    name: { type: String, required: true}
+})
+
+// Rest API
+Client.methods(['get', 'post', 'put', 'delete'])
+Client.updateOptions({new: true, runValidators: true})
+
+//Routes
+Client.register(server, '/clients')
+
 
 // start server 3000
 server.listen(3000)
